0a84dfa41d581b3999794381cea03f4a62d9c300
Comparini <alecomparini.dev@gmail.com>
2023-04-26 21:55:10 -0300

    included components and first screen

--- Smaapper/Home/HomeVC.swift
+++ Smaapper/Home/HomeVC.swift
@@ -9,6 +9,17 @@ import UIKit
 
 class HomeVC: UIViewController {
 
+    lazy var home: Home = {
+        let home = Home()
+        home.translatesAutoresizingMaskIntoConstraints = false
+        return home
+    }()
+    
+    override func loadView() {
+        super.loadView()
+        view = home
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
