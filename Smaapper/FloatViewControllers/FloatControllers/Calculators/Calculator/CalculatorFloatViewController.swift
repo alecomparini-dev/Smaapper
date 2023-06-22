0b9fb8db7356538d9b1e6f8a6d97d1d74a110638
Comparini <alecomparini.dev@gmail.com>
2023-06-22 09:24:57 -0300

    initiate refactor

--- Smaapper/FloatViewControllers/FloatControllers/Calculators/Calculator/CalculatorFloatViewController.swift
+++ Smaapper/FloatViewControllers/FloatControllers/Calculators/Calculator/CalculatorFloatViewController.swift
@@ -11,10 +11,10 @@ class CalculatorFloatViewController: FloatViewController {
     static let identifierApp = "calculator"
     
     private var operation: CalculatorOperationProtocol?
-    private var numberOfDisplay: Double = 0.0
     
     private let calc: Calculation = Calculation()
     private let numberFormatter: NumberFormatterBuilder = NumberFormatterBuilder()
+    private var isPercentageTapped: Bool = false
     private var isClearDisplay: Bool = false
     private var isCurrentValue: Bool = false
     
@@ -52,6 +52,8 @@ class CalculatorFloatViewController: FloatViewController {
             .setNumberStyle(.decimal)
             .setMinimumFractionDigits(0)
             .setMaximumFractionDigits(5)
+        numberFormatter.usesSignificantDigits = true
+        numberFormatter.maximumSignificantDigits = 5
     }
     
     private func configDelegate() {
@@ -67,11 +69,200 @@ class CalculatorFloatViewController: FloatViewController {
         UtilsFloatView.removeShadowActiveFloatView(screen)
     }
     
+    private func setPlusMinus() {
+        var value = numberFormatter.getNumber(getDisplayValue)?.doubleValue ?? 0
+        value = value * -1
+        clearDisplay()
+        setValue(numberFormatter.getString(value) ?? "0")
+    }
+    
+    private func configPercentage() {
+        isClearDisplay = true
+        if isPercentageTapped { return }
+        if isDisplayZero() {return}
+        
+        let operation = calc.operation
+        let num1 = calc.previousValue
+        let num2 = calc.currentValue
+        
+        //Calculando Percentagem
+        var basePercentage = num1
+        if operation != nil {
+            basePercentage = num2
+        }
+        calc.previousValue = basePercentage
+        calc.currentValue = 0.01
+        setOperation(PercentageOperation())
+        makeCalculation()
+        
+        if (operation is AdditionOperation) || (operation is SubtractionOperation) {
+            calc.previousValue = num1
+            calc.currentValue = calc.result
+            setOperation(MultiplicationOperation())
+            makeCalculation()
+            
+            calc.operation = operation
+            calc.previousValue = num1
+            calc.currentValue = calc.result
+            makeCalculation()
+        }
+        
+        if (operation is MultiplicationOperation) || (operation is DivisionOperation) {
+            calc.previousValue = num1
+            calc.currentValue = calc.result
+            setOperation(operation)
+            makeCalculation()
+        }
+        
+        isPercentageTapped = true
+        
+    }
+    
+    private func setValue(_ value: String) {
+        isPercentageTapped = false
+        configValueToDisplay(value)
+        setNumberToCalculation()
+    }
+    
+    private func setEquals() {
+        makeCalculation()
+    }
+    
+    private var getDisplayValue: String {
+        if let display = screen.display.getText {
+            return display
+        }
+        return "0"
+    }
+    
+    private func calculate() -> Double? {
+        let result = calc.calculate
+        if result == nil {
+            calc.previousValue = Double(getDisplayValue)
+        }
+        return result
+    }
+    
+    private var setBackspace: Void {
+        if isDisplayZero() {return}
+        let display = getDisplayValue
+        clearDisplay()
+        if display.count > 1 {
+            configValueToDisplay(String(display.dropLast()))
+        }
+        if display.count == 1 {
+            calc.currentValue = nil
+        }
+        return
+    }
+    
+    private func resetCalculator() {
+        calc.previousValue = nil
+        calc.currentValue = nil
+        calc.operation = nil
+        operation = nil
+        clearDisplay()
+    }
+    
+    private func clearDisplay() {
+        setValueOnDisplay("0")
+    }
+    
+    private var setDecimalSeparator: Void {
+        configValueToDisplay(",")
+    }
+       
+    private func isDisplayZero() -> Bool {
+        let display = screen.display.getText ?? "0"
+        if Double(display) == 0 {
+            return true
+        }
+        return false
+    }
+    
+    private func setValueOnDisplay(_ value: String) {
+        let decimalSeparator = NumberFormatterBuilder.get.decimalSeparator ?? "."
+//        var valueFormatted = numberFormatter.getString(value) ?? "0"
+//        if value.contains(decimalSeparator) {
+//            valueFormatted = value
+//        }
+        
+        screen.display.setText(value.replacingOccurrences(of: ".", with: NumberFormatterBuilder.get.decimalSeparator))
+    }
+    
+    private func configValueToDisplay(_ value: String) {
+        guard var display = screen.display.getText else { return }
+        if isClearDisplay {
+            clearDisplay()
+            isClearDisplay = false
+            display = ""
+        }
+        
+        if isDisplayZero() {display = ""}
+        
+        if value == NumberFormatterBuilder.get.decimalSeparator {
+            display += setDecimalSeparatorOnDisplay()
+        } else {
+            display += value
+        }
+        setValueOnDisplay(display)
+    }
+    
+    private func setDecimalSeparatorOnDisplay() -> String {
+        let display = screen.display.getText ?? ""
+        if display.contains(NumberFormatterBuilder.get.decimalSeparator) { return "" }
+        if isDisplayZero() {
+            return "0\(NumberFormatterBuilder.get.decimalSeparator ?? ".")"
+        }
+        return NumberFormatterBuilder.get.decimalSeparator
+    }
+    
+    private func setNumberToCalculation() {
+        let numberDisplay: Double = numberFormatter.getNumber(getDisplayValue)?.doubleValue ?? 0
+//        let numberDisplay: Double = Double(truncating: numberFormatter.getNumber(getDisplayValue) ?? 0)
+        if operation == nil {
+            calc.previousValue = numberDisplay
+        } else {
+            calc.currentValue = numberDisplay
+        }
+    }
+    
+    private func configCalculation(_ operation: CalculatorOperationProtocol) {
+        if calc.previousValue == nil {
+            calc.previousValue = Double(getDisplayValue)
+        }
+        makeCalculation()
+        setOperation(operation)
+        isClearDisplay = true
+    }
+    
+    private func setOperation(_ operation: CalculatorOperationProtocol?) {
+        if let operation {
+            self.operation = operation
+            self.calc.operation = operation
+        }
+    }
+    
+    private func makeCalculation() {
+        if let result = calc.calculate {
+            setValueOnDisplay("\(result)")
+            operation = nil
+            calc.previousValue = result
+        }
+    }
+    
+    
+    
+    
+    
+    
     
     
     
     
 }
+
+
 //  MARK: - EXTENSION WeatherViewDelegate
 
 extension CalculatorFloatViewController: CalculatorViewDelegate {
@@ -158,182 +349,5 @@ extension CalculatorFloatViewController: CalculatorButtonsViewDelegate {
                 
         }
     }
-    
-    private func setPlusMinus() {
-        var value = numberFormatter.getNumber(getDisplayValue)?.doubleValue ?? 0
-        value = value * -1
-        setValue("\( value)")
-    }
-    
-    private func configPercentage() {
-        isClearDisplay = true
-        if isDisplayZero() {return}
-        if calc.operation is PercentageOperation {return}
-        
-        let operation = calc.operation
-        let num1 = calc.previousValue
-        let num2 = calc.currentValue
-        
-        //Calculando Percentagem
-        var basePercentage = num1
-        if operation != nil {
-            basePercentage = num2
-        }
-        calc.previousValue = basePercentage
-        calc.currentValue = 0.01
-        calc.operation = PercentageOperation()
-        makeCalculation()
-        
-        if (operation is AdditionOperation) || (operation is SubtractionOperation) {
-            calc.previousValue = num1
-            calc.currentValue = calc.result
-            calc.operation = MultiplicationOperation()
-            makeCalculation()
-            
-            calc.operation = operation
-            calc.previousValue = num1
-            calc.currentValue = calc.result
-            makeCalculation()
-        }
-        
-        if (operation is MultiplicationOperation) || (operation is DivisionOperation) {
-            calc.previousValue = num1
-            calc.currentValue = calc.result
-            calc.operation = operation
-            makeCalculation()
-        }
-        
-        calc.operation = PercentageOperation()
-        
-    }
-    
-    private func setValue(_ value: String) {
-        setValueOnDisplay(value)
-        setDisplayToCalculation()
-    }
-    
-    private func setEquals() {
-        makeCalculation()
-    }
-    
-    private var getDisplayValue: String {
-        if let display = screen.display.getText {
-            return display
-        }
-        return "0"
-    }
-    
-    private func calculate() -> Double? {
-        let result = calc.calculate
-        if result == nil {
-            calc.previousValue = Double(getDisplayValue)
-        }
-        return result
-    }
-    
-    private var setBackspace: Void {
-        if isDisplayZero() {return}
-        let display = getDisplayValue
-        clearDisplay()
-        if display.count > 1 {
-            setValueOnDisplay(String(display.dropLast()))
-        }
-        if display.count == 1 {
-            calc.currentValue = nil
-        }
-        return
-    }
-    
-    private func resetCalculator() {
-        calc.previousValue = nil
-        calc.currentValue = nil
-        calc.operation = nil
-        operation = nil
-        isClearDisplay = false
-        clearDisplay()
-    }
-    
-    private func clearDisplay() {
-        screen.display.setText("0")
-    }
-    
-    private var setDecimalSeparator: Void {
-        setValueOnDisplay(",")
-    }
-       
-    private func isDisplayZero() -> Bool {
-        let display = screen.display.getText ?? "0"
-        if Double(display) == 0 {
-            return true
-        }
-        return false
-    }
-    
-    private func setResultOnDisplay(_ value: String) {
-        screen.display.setText(numberFormatter.getString(value) ?? "0")
-    }
-    
-    private func setValueOnDisplay(_ value: String) {
-        guard var display = screen.display.getText else { return }
-        if isClearDisplay {
-            clearDisplay()
-            isClearDisplay = false
-            display = ""
-        }
-        
-        if isDisplayZero() {display = ""}
-        
-        if value == NumberFormatterBuilder.get.decimalSeparator {
-            display += setDecimalOnDisplay()
-        } else {
-            display += value
-        }
-        screen.display.setText(display)
-        setDisplayToCalculation()
-    }
-    
-    private func setDecimalOnDisplay() -> String {
-        let display = screen.display.getText ?? ""
-        if display.contains(NumberFormatterBuilder.get.decimalSeparator) { return ""}
-        if isDisplayZero() {
-            return "0\(NumberFormatterBuilder.get.decimalSeparator ?? ".")"
-        }
-        return NumberFormatterBuilder.get.decimalSeparator
-    }
-    
-    private func setDisplayToCalculation() {
-        let display = getDisplayValue
-        if operation == nil {
-            calc.previousValue = numberFormatter.getNumber(display)?.doubleValue
-        } else {
-            calc.currentValue = numberFormatter.getNumber(display)?.doubleValue
-        }
-    }
-    
-    private func configCalculation(_ operation: CalculatorOperationProtocol) {
-        if calc.previousValue == nil {
-            calc.previousValue = Double(getDisplayValue)
-        }
-        makeCalculation()
-        setOperation(operation)
-        isClearDisplay = true
-    }
-    
-    private func setOperation(_ operation: CalculatorOperationProtocol) {
-        if calc.previousValue == nil {
-            calc.previousValue = 0
-        }
-        self.operation = operation
-        self.calc.operation = operation
-    }
-    
-    private func makeCalculation() {
-        if let result = calc.calculate {
-            setResultOnDisplay("\(result)")
-            operation = nil
-            calc.previousValue = result
-        }
-    }
-
     
 }
