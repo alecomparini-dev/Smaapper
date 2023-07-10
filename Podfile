d072cfcbbafe23f8135a5b276c353838751473eb
Comparini <alecomparini.dev@gmail.com>
2023-07-10 18:29:36 -0300

    included Realm via cocoapods

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '12.0'
+
+target 'Smaapper' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Smaapper
+  pod 'RealmSwift', '~>10.41'
+
+
+end
---
+++ Smaapper/Resources/Components/PersistenceData/PersistenceDataBuilder.swift
@@ -0,0 +1,19 @@
+//
+//  PersistenceDataBuilder.swift
+//  Smaapper
+//
+//  Created by Alessandro Comparini on 10/07/23.
+//
+
+import Foundation
+
+/*
+ 1 - tudo que for gravar usar o ID do usuário
+ 2 - Para pegar o arquivo do Realm (Realm.Configuration.defaultConfiguration.fileURL)
+ */
+
+class PersistenceDataBuilder {
+    
+    
+    
+}
