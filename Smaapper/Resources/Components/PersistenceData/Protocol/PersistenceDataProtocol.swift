0c2e286fea6050278109b168a369ffcc71ff1ab1
Comparini <alecomparini.dev@gmail.com>
2023-07-11 19:59:39 -0300

    tests with async

--- Smaapper/Resources/Components/PersistenceData/Protocol/PersistenceDataProtocol.swift
+++ Smaapper/Resources/Components/PersistenceData/Protocol/PersistenceDataProtocol.swift
@@ -12,12 +12,12 @@ import Foundation
 protocol PersistenceDataProtocol {
     associatedtype Entity: Identifiable<UUID>
     
-    func insert(_ entity: Entity) throws -> UUID
-    func update(_ entity: Entity) throws
-    func delete(_ entity: Entity) throws
-    func fetch() throws -> [Entity]
-    func findByID(_ id: UUID) throws -> Entity?
-    func findByColumn<DataType>(column: String, value: DataType) throws -> [Entity]
+    func insert(_ entity: Entity) async throws -> UUID
+    func update(_ entity: Entity) async throws
+    func delete(_ entity: Entity) async throws
+    func fetch() async throws -> [Entity]
+    func findByID(_ id: UUID) async throws -> Entity?
+    func findByColumn<DataType>(column: String, value: DataType) async throws -> [Entity]
 }
 
 
