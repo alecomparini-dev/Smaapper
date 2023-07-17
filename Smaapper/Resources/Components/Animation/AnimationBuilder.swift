635e8c10b45bcd7a997491760efbd6f41c0d72a8
Comparini <alecomparini.dev@gmail.com>
2023-07-17 18:15:41 -0300

    refactor

---
+++ Smaapper/Resources/Components/Animation/AnimationBuilder.swift
@@ -0,0 +1,23 @@
+//
+//  AnimationBuilder.swift
+//  Smaapper
+//
+//  Created by Alessandro Comparini on 17/07/23.
+//
+
+import UIKit
+
+class AnimationBuilder {
+    
+    private(set) var transition: CATransition?
+    
+    
+    func setTransitionAnimation() {
+        
+    }
+    
+    func setBasicAnimation() {
+        
+    }
+    
+}
